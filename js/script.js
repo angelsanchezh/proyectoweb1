96510a2bf927aab13af5a37f82787b5a1c78ca8a
angelsanchezh <angelsanchezh@gmail.com>
2022-07-19 21:35:59 -0500

    js

--- js/script.js
+++ js/script.js
@@ -10,20 +10,41 @@ var caja_trasera_login = document.querySelector(".caja__trasera-login");
 var caja_trasera_register = document.querySelector(".caja__trasera-register");
 
 function iniciarsesion(){
+
+if(window.innerWidth > 850){
     formulario_register.style.display = "none";
     contenedor_login_register.style.left = "10px";
     formulario_login.style.display = "block";
     caja_trasera_register.style.opacity = "1";
     caja_trasera_login.style.opacity = "0";
+}else{
+    formulario_register.style.display = "none";
+    contenedor_login_register.style.left = "0px";
+    formulario_login.style.display = "block";
+    caja_trasera_register.style.display = "block";
+    caja_trasera_login.style.display = "none";
 }
 
+}
+
+
 function register(){
+
+    if(window.innerWidth > 850){
+
     formulario_register.style.display = "block";
     contenedor_login_register.style.left = "410px";
     formulario_login.style.display = "none";
     caja_trasera_register.style.opacity = "0";
     caja_trasera_login.style.opacity = "1";
+    }else{
 
-
+        formulario_register.style.display = "block";
+    contenedor_login_register.style.left = "0px";
+    formulario_login.style.display = "none";
+    caja_trasera_register.style.display = "none";
+    caja_trasera_login.style.display = "block";
+caja_trasera_login.style.opacity = "1"
+}
 
 }
